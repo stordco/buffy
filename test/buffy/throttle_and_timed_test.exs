4f330f162937c0e23cd403c0dd969a4dac04744f
Seungjin Kim <seungjin.kim@stord.com>
2023-12-12 16:40:50 -0800

    UPDATE: rewrite the tests so that it uses assert_receive

--- test/buffy/throttle_and_timed_test.exs
+++ test/buffy/throttle_and_timed_test.exs
@@ -1,28 +1,63 @@
 defmodule Buffy.ThrottleAndTimedTest do
   use ExUnit.Case, async: true
-  use Patch
   use ExUnitProperties
 
-  setup do
-    spy(UsingThrottleAndTimedZeroThrottler)
-    spy(UsingThrottleAndTimedSlowThrottler)
-    :ok
+  import ExUnit.CaptureLog
+
+  defmodule MyDynamicSupervisor do
+    use DynamicSupervisor
+
+    def start_link(init_arg) do
+      DynamicSupervisor.start_link(__MODULE__, init_arg, name: __MODULE__)
+    end
+
+    @impl DynamicSupervisor
+    def init(_init_arg) do
+      DynamicSupervisor.init(strategy: :one_for_one)
+    end
+  end
+
+  defmodule MySlowThrottler do
+    use Buffy.ThrottleAndTimed,
+      throttle: 100,
+      supervisor_module: DynamicSupervisor,
+      supervisor_name: MyDynamicSupervisor
+
+    def handle_throttle(:raise) do
+      raise RuntimeError, message: ":raise"
+    end
+
+    def handle_throttle(:error) do
+      :error
+    end
+
+    def handle_throttle(%{test_pid: test_pid} = args) do
+      send(test_pid, {:ok, args, System.monotonic_time()})
+      :ok
+    end
+  end
+
+  defmodule MyZeroThrottler do
+    use Buffy.Throttle,
+      throttle: 0,
+      supervisor_module: DynamicSupervisor,
+      supervisor_name: MyDynamicSupervisor
+
+    def handle_throttle(:raise) do
+      raise RuntimeError, message: ":raise"
+    end
+
+    def handle_throttle(:error) do
+      :error
+    end
+
+    def handle_throttle(%{test_pid: test_pid} = args) do
+      send(test_pid, {:ok, args, System.monotonic_time()})
+      :ok
+    end
   end
 
   describe "handle_info(:timeout)" do
-    defmodule MyDynamicSupervisor do
-      use DynamicSupervisor
-
-      def start_link(init_arg) do
-        DynamicSupervisor.start_link(__MODULE__, init_arg, name: __MODULE__)
-      end
-
-      @impl DynamicSupervisor
-      def init(_init_arg) do
-        DynamicSupervisor.init(strategy: :one_for_one)
-      end
-    end
-
     defmodule MyTimedThrottler do
       use Buffy.ThrottleAndTimed,
         throttle: 10,
@@ -136,22 +171,28 @@ defmodule Buffy.ThrottleAndTimedTest do
     end
   end
 
-  # Extend timeout for the number of CI runs + the Process.sleep call.
-  # Because MyZeroDebouncer.debounce/1 is async, we need to sleep to ensure
-  # the logic is ran.
-  @tag timeout: :timer.minutes(10)
-  test "calls handle_throttle/1" do
-    check all args <- StreamData.term() do
-      assert :ok = UsingThrottleAndTimedZeroThrottler.throttle(args)
-      Process.sleep(1)
-      assert_called UsingThrottleAndTimedZeroThrottler.handle_throttle(args)
+  describe "handle_throttle/1" do
+    setup do
+      start_supervised!({MyDynamicSupervisor, []})
+      :ok
     end
-  end
 
-  test "throttles handle_throttle/1" do
-    for _ <- 1..200, do: UsingThrottleAndTimedSlowThrottler.throttle(:testing)
-    Process.sleep(100)
-    assert_called_once UsingThrottleAndTimedSlowThrottler.handle_throttle(:testing)
+    # Extend timeout for the number of CI runs + the Process.sleep call.
+    # Because MyZeroDebouncer.debounce/1 is async, we need to sleep to ensure
+    # the logic is ran.
+    @tag timeout: :timer.minutes(10)
+    test "calls handle_throttle/1" do
+      check all args <- StreamData.term() do
+        assert :ok = MyZeroThrottler.throttle(%{args: args, test_pid: self()})
+        assert_receive {:ok, _, _}
+      end
+    end
+
+    test "throttles handle_throttle/1" do
+      test_pid = self()
+      for _ <- 1..200, do: MySlowThrottler.throttle(%{test_pid: test_pid})
+      assert_receive {:ok, _, _}, 200
+    end
   end
 
   describe ":telemetry" do
@@ -164,54 +205,46 @@ defmodule Buffy.ThrottleAndTimedTest do
           [:buffy, :throttle, :handle, :exception]
         ])
 
+      start_supervised!({MyDynamicSupervisor, []})
+
       :ok
     end
 
     test "emits [:buffy, :throttle, :throttle]" do
-      UsingThrottleAndTimedZeroThrottler.throttle(:foo)
+      MyZeroThrottler.throttle(:foo)
 
       assert_receive {[:buffy, :throttle, :throttle], _ref, %{count: 1},
                       %{
                         args: :foo,
                         key: _,
-                        module: UsingThrottleAndTimedZeroThrottler
+                        module: MyZeroThrottler
                       }}
     end
 
     test "emits [:buffy, :throttle, :handle, :start]" do
-      UsingThrottleAndTimedZeroThrottler.throttle(:starting)
+      MyZeroThrottler.throttle(:starting)
 
       assert_receive {[:buffy, :throttle, :handle, :start], _ref, %{},
                       %{
                         args: :starting,
                         key: _,
-                        module: UsingThrottleAndTimedZeroThrottler
-                      }}
-    end
-
-    test "emits [:buffy, :throttle, :handle, :stop]" do
-      UsingThrottleAndTimedZeroThrottler.throttle(:stopping)
-
-      assert_receive {[:buffy, :throttle, :handle, :stop], _ref, %{duration: _},
-                      %{
-                        args: :stopping,
-                        key: _,
-                        result: :ok,
-                        module: UsingThrottleAndTimedZeroThrottler
+                        module: MyZeroThrottler
                       }}
     end
 
     test "emits [:buffy, :throttle, :handle, :exception]" do
-      UsingThrottleAndTimedZeroThrottler.throttle(:raise)
+      MyZeroThrottler.throttle(:raise)
 
-      assert_receive {[:buffy, :throttle, :handle, :exception], _ref, %{duration: _},
-                      %{
-                        args: :raise,
-                        key: _,
-                        kind: :error,
-                        reason: %RuntimeError{message: ":raise"},
-                        module: UsingThrottleAndTimedZeroThrottler
-                      }}
+      assert capture_log(fn ->
+               assert_receive {[:buffy, :throttle, :handle, :exception], _ref, %{duration: _},
+                               %{
+                                 args: :raise,
+                                 key: _,
+                                 kind: :error,
+                                 reason: %RuntimeError{message: ":raise"},
+                                 module: MyZeroThrottler
+                               }}
+             end)
     end
   end
 end
