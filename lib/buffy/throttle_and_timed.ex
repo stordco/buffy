d7c3f859170270667e4d94523cf8e43a742257b0
Sam Hunter <1526888+kinson@users.noreply.github.com>
2023-12-18 08:35:42 -0800

    chore: Remove Self Alias (#33)

--- lib/buffy/throttle_and_timed.ex
+++ lib/buffy/throttle_and_timed.ex
@@ -158,7 +158,6 @@ defmodule Buffy.ThrottleAndTimed do
 
   """
   require Logger
-  alias Buffy.ThrottleAndTimed
 
   @typedoc """
   A list of arbitrary arguments that are used for the `c:handle_throttle/1`
@@ -204,14 +203,15 @@ defmodule Buffy.ThrottleAndTimed do
     loop_interval = Keyword.fetch!(opts, :loop_interval)
 
     quote do
-      @behaviour ThrottleAndTimed
+      @behaviour Buffy.ThrottleAndTimed
 
       use GenServer, restart: unquote(restart)
 
       require Logger
 
       @doc false
-      @spec start_link({ThrottleAndTimed.key(), ThrottleAndTimed.args()}) :: :ignore | {:ok, pid} | {:error, term()}
+      @spec start_link({Buffy.ThrottleAndTimed.key(), Buffy.ThrottleAndTimed.args()}) ::
+              :ignore | {:ok, pid} | {:error, term()}
       def start_link({key, args}) do
         name = key_to_name(key)
 
@@ -231,8 +231,8 @@ defmodule Buffy.ThrottleAndTimed do
           {:ok, #PID<0.123.0>}
 
       """
-      @impl ThrottleAndTimed
-      @spec throttle(ThrottleAndTimed.args()) :: :ok | {:error, term()}
+      @impl Buffy.ThrottleAndTimed
+      @spec throttle(Buffy.ThrottleAndTimed.args()) :: :ok | {:error, term()}
       def throttle(args) do
         key = args_to_key(args)
 
@@ -286,8 +286,8 @@ defmodule Buffy.ThrottleAndTimed do
           end
 
       """
-      @impl ThrottleAndTimed
-      @spec handle_throttle(ThrottleAndTimed.args()) :: any()
+      @impl Buffy.ThrottleAndTimed
+      @spec handle_throttle(Buffy.ThrottleAndTimed.args()) :: any()
       def handle_throttle(_args) do
         raise RuntimeError,
           message: "You must implement the `handle_throttle/1` function in your module."
@@ -297,7 +297,7 @@ defmodule Buffy.ThrottleAndTimed do
 
       @doc false
       @impl GenServer
-      @spec init({ThrottleAndTimed.key(), ThrottleAndTimed.args()}) :: {:ok, ThrottleAndTimed.state()}
+      @spec init({Buffy.ThrottleAndTimed.key(), Buffy.ThrottleAndTimed.args()}) :: {:ok, Buffy.ThrottleAndTimed.state()}
       def init({key, args}) do
         {:ok, schedule_throttle_and_update_state(%{key: key, args: args, timer_ref: nil})}
       end
@@ -308,7 +308,7 @@ defmodule Buffy.ThrottleAndTimed do
 
       """
       @impl GenServer
-      @spec handle_cast(:throttle, ThrottleAndTimed.state()) :: {:noreply, ThrottleAndTimed.state()}
+      @spec handle_cast(:throttle, Buffy.ThrottleAndTimed.state()) :: {:noreply, Buffy.ThrottleAndTimed.state()}
       def handle_cast(:throttle, %{timer_ref: nil} = state) do
         {:noreply, schedule_throttle_and_update_state(state)}
       end
@@ -324,8 +324,8 @@ defmodule Buffy.ThrottleAndTimed do
 
       @doc false
       @impl GenServer
-      @spec handle_info(:timeout | :execute_throttle_callback, ThrottleAndTimed.state()) ::
-              {:noreply, ThrottleAndTimed.state(), {:continue, :do_work}}
+      @spec handle_info(:timeout | :execute_throttle_callback, Buffy.ThrottleAndTimed.state()) ::
+              {:noreply, Buffy.ThrottleAndTimed.state(), {:continue, :do_work}}
       def handle_info(:timeout, %{key: key, args: args} = state) do
         :telemetry.execute(
           [:buffy, :throttle, :timeout],
@@ -342,13 +342,8 @@ defmodule Buffy.ThrottleAndTimed do
 
       @doc false
       @impl GenServer
-      @spec handle_continue(do_work :: atom(), ThrottleAndTimed.state()) ::
-              {:noreply,
-               %{
-                 args: ThrottleAndTimed.args(),
-                 key: ThrottleAndTimed.key(),
-                 timer_ref: nil | reference()
-               }, timeout()}
+      @spec handle_continue(do_work :: atom(), Buffy.ThrottleAndTimed.state()) ::
+              {:noreply, Buffy.ThrottleAndTimed.state()} | {:noreply, Buffy.ThrottleAndTimed.state(), timeout()}
       def handle_continue(:do_work, %{key: key, args: args} = state) do
         :telemetry.span(
           [:buffy, :throttle, :handle],
