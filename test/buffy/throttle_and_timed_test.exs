494aeee4bd2c7632f3422dd89dc246af045a2e87
Seungjin Kim <seungjin.kim@stord.com>
2024-02-23 19:01:47 -0800

    UPDATE: tests fixed

--- test/buffy/throttle_and_timed_test.exs
+++ test/buffy/throttle_and_timed_test.exs
@@ -175,6 +175,65 @@ defmodule Buffy.ThrottleAndTimedTest do
     end
   end
 
+  describe "usage: collecting args to state and running them when :timeout" do
+    defmodule MyTimedSlowBucketingThrottler do
+      @moduledoc """
+      `args` is a map. It will always come in with %{key: "key"}.
+      """
+      use Buffy.ThrottleAndTimed,
+        throttle: 100,
+        loop_interval: 300,
+        supervisor_module: DynamicSupervisor,
+        supervisor_name: MyDynamicSupervisor
+
+      def handle_throttle(%{test_pid: test_pid, values: _values} = args) do
+        send(test_pid, {:ok, args, System.monotonic_time()})
+        :ok
+      end
+
+      def args_to_key(%{key: key}), do: key |> :erlang.term_to_binary() |> :erlang.phash2()
+
+      def update_args(%{values: values} = old_arg, %{values: new_values} = _new_arg)
+          when is_list(values) and is_list(new_values) do
+        %{old_arg | values: Enum.sort(values ++ new_values)}
+      end
+
+      def update_state_with_work_result(%{args: args} = state, _) do
+        %{state | args: %{args | values: []}}
+      end
+    end
+
+    setup do
+      start_supervised!({MyDynamicSupervisor, []})
+      :ok
+    end
+
+    test "should use overrideable functions to use collection of arg values from all of triggers when work is done" do
+      test_pid = self()
+      # trigger throttle
+      MyTimedSlowBucketingThrottler.throttle(%{key: "my_key", test_pid: test_pid, values: [0]})
+
+      # trigger more throttle
+      for x <- 1..10 do
+        Task.async(fn ->
+          MyTimedSlowBucketingThrottler.throttle(%{key: "my_key", test_pid: test_pid, values: [x]})
+        end)
+      end
+
+      # assert throttled work done
+      expected_value = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+      assert_receive {:ok, %{values: ^expected_value}, now}, 200
+
+      # refute any other work was done
+      refute_receive {:ok, _, _now}, 200
+
+      # check inbox timeout triggered
+      assert_receive {:ok, _, now2}, 400
+      diff = System.convert_time_unit(now2 - now, :native, :millisecond)
+      assert :erlang.abs(diff - 300) < 10
+    end
+  end
+
   describe ":telemetry" do
     setup do
       :telemetry_test.attach_event_handlers(self(), [
